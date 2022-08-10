b5881bbc25f5df06fbdd91e84326c738520a189c
Dolorrr Lapule <103165024+yagirldolor@users.noreply.github.com>
2022-08-10 17:40:06 +0000

    Adds Express

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
-var http = require("http");
-http.createServer(function(req,res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Aloha world');
-}).listen(process.env.PORT || 3000);
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Ola from Express!')
+})
+app.get('/about', (req, res) => {
+  res.send('About Page')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
